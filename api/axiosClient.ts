5114fe1feb8d19122a51bfc39b5d254acd75a8ab
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-10-24 13:50:23 +1100

    Add package humps to convert request payloads from coingecko to camelCase

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,7 +1,14 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { camelizeKeys } from "humps";
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+axiosClient.interceptors.response.use((response: AxiosResponse<any>) => {
+  response.data = camelizeKeys(response.data);
+
+  return response;
 });
 
 export default axiosClient;
