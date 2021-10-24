fa48a032bfca6364af1ffc87db3e06f98222a6af
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-10-24 12:10:45 +1100

    Add and setup Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import "../styles/globals.css";
+
 
 const CodingChallenge = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default CodingChallenge;
